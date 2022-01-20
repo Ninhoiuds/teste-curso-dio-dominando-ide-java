3f82eee7a1886f13cdc2c630338b47a1ae880ee8
Ninhoiuds <iudesneider@yahoo.com.br>
2022-01-19 21:22:41 -0300

    first commit

---
+++ src/com/dio/primeiro_programa.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class primeiro_programa {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.print(gato);
+		
+		
+		
+		/*int a = 2;
+int b = 3;
+		System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
+ 
